ad47f27d75e83dd4d98322f7b6f314db6458ba7c
Mahalakshmi K <1476mahalakshmi@gmail.com>
2026-05-01 23:13:59 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,12 +1,14 @@
 from binance.client import Client
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_client():
-    return Client(
-        os.getenv("UiaGZNhZfv03bxGnjVPjWQZy41nGjjU21ZFodEcdkVeIH2RxKuFajZEnOY3eNdcE"),
-        os.getenv("hkqcF71W4Tgy58CwXd6j5Pt20KxC7xk1eXXdhDcJ86zWyMSwrxxXKFmF5myllh1X"),
-        testnet=True
-    )
+    api_key = os.environ.get("UiaGZNhZfv03bxGnjVPjWQZy41nGjjU21ZFodEcdkVeIH2RxKuFajZEnOY3eNdcE")
+    api_secret = os.environ.get("hkqcF71W4Tgy58CwXd6j5Pt20KxC7xk1eXXdhDcJ86zWyMSwrxxXKFmF5myllh1X")
+
+    print("API KEY:", api_key)
+    print("SECRET:", api_secret)
+
+    if not api_key or not api_secret:
+        raise Exception("API keys not found!")
+
+    return Client(api_key, api_secret)
